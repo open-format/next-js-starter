493f79e8ef1b0f971cf991af249c7a3cdaae02a7
Andy <andy@openformat.tech>
2024-11-04 07:23:48 +1100

    adds @privy-io/react-auth and Privy provider

---
+++ src/app/providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { PrivyProvider } from "@privy-io/react-auth";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID as string}
+      config={{
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: "users-without-wallets",
+        },
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  );
+}
